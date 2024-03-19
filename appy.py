17fae75019b86b7448bdb95b25b983e2cef63313
shamithanaik <98634778+shamithanaik@users.noreply.github.com>
2024-03-19 11:19:01 +0530

    Update appy.py

--- appy.py
+++ appy.py
@@ -8,16 +8,27 @@ import requests
 
 url = "https://drive.google.com/file/d/1Vzl8GJsUJr3-wCkL3DcWoFPfJs_Ufi-A/view?usp=sharing"
 
+
+
+
+
 def download_file(url, file_path):
     response = requests.get(url)
-    with open(file_path, "wb") as f:
-        f.write(response.content)
+    if response.status_code == 200:
+        with open(file_path, "wb") as f:
+            f.write(response.content)
+        return True
+    else:
+        return False
 
 predictor_path = "shape_predictor_68_face_landmarks.dat"
 
 if not os.path.exists(predictor_path):
-    download_file(url, predictor_path)
-
+    st.write("Downloading shape predictor file...")
+    if download_file(url, predictor_path):
+        st.write("Shape predictor file downloaded successfully.")
+    else:
+        st.error("Failed to download shape predictor file.")
 
 detector = dlib.get_frontal_face_detector()
 #predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
